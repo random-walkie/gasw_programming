e763642287bef5b72e713507321826ea8236a194
random-walkie <26143642+random-walkie@users.noreply.github.com>
2024-10-02 10:15:12 +0100

    lecture1 script

---
+++ programming_zero/the_end.py
@@ -0,0 +1,15 @@
+a = "This is the end" # This is a string
+b = "The end of"
+c = "friend"
+d = "\n~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n"
+
+year = (2 ** 11 -81) # raise 2 to power 11
+
+print(d + a + " beautiful " + c)
+print(a + " my only " + c)
+
+print(b + " our elaborate plans ")
+print(b + " of everything that stands ")
+
+print("\t\t Album: The Doors")
+print("\t\t Album: Released " + str(year) + d)
