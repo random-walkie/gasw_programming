98450fe89557e7776fcee96e4256aefd69e3cdd3
random-walkie <26143642+random-walkie@users.noreply.github.com>
2024-10-12 16:09:20 +0100

    fixed bug in code - padded input string to the left, not right. finished exercise 1 from challenge 02.

--- programming_zero/practicals/code/challenge_02/to_integer_1.py
+++ programming_zero/practicals/code/challenge_02/to_integer_1.py
@@ -1,7 +1,7 @@
-def to_integer_2(binary_string):
+def to_integer_1(binary_string):
     """
     This function converts binary strings (up to 8 bits) into its
-    corresponding integer value, using a for loop.
+    corresponding integer value, using conditional logic.
     
     Parameters:
     binary_string (str): Binary string.
@@ -23,27 +23,37 @@ def to_integer_2(binary_string):
         # the string is less than 8
         if string_length < bits:
             binary_string = "0" * (bits - string_length) + binary_string
-        # Below I am getting the exponent to calculate the 
-        # integer value corresponding to the binary number.
-        # E.g. 111:
-        # 1 x 2 x 2 (or 2^2) = 4
-        # 1 x 2 (or 2^1) = 2
-        # 1 x 1 (or 2^0) = 1
-        # So 111 -> 4 + 2 + 1 = 7
-        exponent = len(binary_string) - 1 # need to subtract 1 to lenght, because we have exponent 0
+                
         integer = 0
-        # Now iterate over each character in the string
-        for char in binary_string:
-            # Then calculate as explained above
-            integer += int(char) * 2**exponent
-            # Need to subtract 1 to the exponent in each iteration
-            exponent -= 1
+        if binary_string[0] == "1":
+            integer += 2**7
+        
+        if binary_string[1] == "1":
+            integer += 2**6
+        
+        if binary_string[2] == "1":
+            integer += 2**5
+        
+        if binary_string[3] == "1":
+            integer += 2**4
+        
+        if binary_string[4] == "1":
+            integer += 2**3
+        
+        if binary_string[5] == "1":
+            integer += 2**2
+        
+        if binary_string[6] == "1":
+            integer += 2**1
+        
+        if binary_string[7] == "1":
+            integer += 2**0
                 
         return integer
 
 # Main block to test the function
 if __name__ == "__main__":
     binary_string = "011"
-    result = to_integer_2(binary_string)
-    print(f"The result is: {result}")
+    result = to_integer_1(binary_string)
+    print(f"The corresponding integer is: {result}")
     
